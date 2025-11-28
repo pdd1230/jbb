990f7b883f1efb3300b3c0e816babda4318052c6
pdd1230 <laki@naver.com>
2025-11-28 12:01:08 +0900

    List 인터페이스 사용

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
+import java.util.List;       // Ctrl + Alt + L --> 자동 import , 포맷 재구성
 
 public class Main {
     public static void main(String[] args) {
 //        exam01();
 //        exam02();
-        exam03();
+//        exam03();
+        exam04();
     }
 
 
-    static void exam01()  {   //articleLength 변수 생성, for문을 이용해서 NPE 회피 --> 리모델링 필요
-        int articleLength = 0 ;
+    static void exam01() {   //articleLength 변수 생성, for문을 이용해서 NPE 회피 --> 리모델링 필요
+        int articleLength = 0;
         Article[] articles = new Article[10];
 
         articles[0] = new Article();
@@ -25,11 +27,12 @@ public class Main {
         articles[5] = new Article();
         articleLength++;
 
-        for(int i = 0 ; i < articleLength; i++ ) {
+        for (int i = 0; i < articleLength; i++) {
             Article article = articles[i];
             System.out.println(article.id);
         }
     }
+
     static void exam02() {
         ArrayList articles = new ArrayList();   //ArrayList
 //        articles.add(articles);               //Object는 모든 타입을 받을 수 있음
@@ -46,11 +49,12 @@ public class Main {
 
         System.out.println(articles);  // toString() 호출 , List는 [o , o, o] 형태로 출력됨
 
-        for (int i =0 ; i < articles.size() ; i++ ) {   // size() --> ArrayList 타입의 배열의 크기를 가져옴
-            Article article =(Article) articles.get(i);  //강제 형변환,   get(i)는 return 타입이 Object(모든 class 부모)
+        for (int i = 0; i < articles.size(); i++) {   // size() --> ArrayList 타입의 배열의 크기를 가져옴
+            Article article = (Article) articles.get(i);  //강제 형변환,   get(i)는 return 타입이 Object(모든 class 부모)
             System.out.println();
         }
     }
+
     static void exam03() {
         ArrayList<Article> articles = new ArrayList<>();  //제너릭 사용 --> 'E' Article 지정       articles.add(new Article());
         articles.add(new Article());
@@ -59,8 +63,8 @@ public class Main {
         articles.add(new Article());
         articles.add(new Article());
 
-        for(Article article : articles) {   //향상된 for문도 잘 수행
-           System.out.println(article.id);
+        for (Article article : articles) {   //향상된 for문도 잘 수행
+            System.out.println(article.id);
         }
 
 //        for (int i =0 ; i < articles.size() ; i++ ) {
@@ -70,24 +74,46 @@ public class Main {
 
     }
 
+    static void exam04() {     // List,  ArrayList 사용함
+        List<Article> articles = new ArrayList<>();
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+
+        for (Article article : articles) {   //향상된 for문도 잘 수행
+            System.out.println(article.id);
+        }
+
+//        for (int i =0 ; i < articles.size() ; i++ ) {
+//            Article article = articles.get(i);    // 강제형변환을 안해도 된다
+//            System.out.println(article.id);
+//        }
+
+    }
+
+
+
+
 }
 
 class Article {
-        static int lastId;
-        int id;
+    static int lastId;
+    int id;
 
-        static {
-            lastId = 0;
-        }
+    static {
+        lastId = 0;
+    }
 
-        Article() {
-            this(lastId + 1);
-            lastId++;
-        }
+    Article() {
+        this(lastId + 1);
+        lastId++;
+    }
 
-        Article(int id) {
-            this.id = id;
-        }
+    Article(int id) {
+        this.id = id;
+    }
 
     @Override
     public String toString() {
