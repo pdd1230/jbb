f106714441cbdcd0b42b7f64aff513efe8abf4f3
pdd1230 <laki@naver.com>
2025-12-01 13:14:53 +0900

    article write 구현중

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -6,17 +6,35 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
         Scanner sc = new Scanner(System.in);
+        int id = 1;  //게시글의 번호를 저장
         while (true) {
             System.out.printf("cmd) ");
-            String cmd = sc.next();
+            String cmd = sc.nextLine().trim();   //trim() 공부 --> 사용
             System.out.println("명령어) " + cmd);
-//            if (cmd == "exit") {   // 주소값이 값은지
-//                break;
-//            }
+
+            if ( cmd.length() == 0) {
+                System.out.println("명령어를 입력해 주세요");
+                continue;        // 반복문 처음부터 다시 진행
+            }
+
+            if ( cmd.equals("article list")) {
+                System.out.println("게시글이 없습니다");
+            } else if (cmd.equals("article write")) {
+                System.out.print("제목 :");
+                String title = sc.nextLine();
+                System.out.println("내용 :");
+                String body = sc.nextLine();
+                System.out.println(id + " 번글이 생성되었습니다");
+                id++;
+            } else {
+                System.out.println("존재하지 않는 명령어 입니다");
+            }
 
             if (cmd.equals("exit")) { //cmd 객체의 값이 "exit" 같은지
                 break;                //반복문을 빠져나감
             }
+
+
         }
 
         System.out.println("== 프로그램 종료 ==");
