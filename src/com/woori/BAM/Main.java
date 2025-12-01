ee3ebdc72da48127fb926e4505aaf2631cb74482
pdd1230 <laki@naver.com>
2025-12-01 11:17:21 +0900

    무한루프를 exit로 빠져나가기 / break

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -6,10 +6,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
         Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.printf("cmd) ");
+            String cmd = sc.next();
+            System.out.println("명령어) " + cmd);
+//            if (cmd == "exit") {   // 주소값이 값은지
+//                break;
+//            }
 
-        System.out.printf("cmd) ");
-        String cmd = sc.nextLine();
-        System.out.println("명령어) " + cmd);
+            if (cmd.equals("exit")) { //cmd 객체의 값이 "exit" 같은지
+                break;                //반복문을 빠져나감
+            }
+        }
+
         System.out.println("== 프로그램 종료 ==");
     }
 
