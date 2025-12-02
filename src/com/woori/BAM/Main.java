e80df85120e212d9d2e9ffe680dc23edf50da0c4
pdd1230 <laki@naver.com>
2025-12-02 15:08:06 +0900

    regDate 날짜와 시간 추가/ Util class 이용

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -1,5 +1,4 @@
 package com.woori.BAM;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
         Scanner sc = new Scanner(System.in);
-        int lastArticleID  = 1;       //마지막 article 번호 저장용 사용
-        List<Article> articles = new ArrayList<>();  // 변수 articles 의 타입은 --> 제너릭 <article>
-                                                     // ArrayList --> List를 구현하는 구현 class
+        int lastArticleID = 1;
+        List<Article> articles = new ArrayList<>();
+
         while (true) {
             System.out.printf("cmd) ");
             String cmd = sc.nextLine().trim();
-//            System.out.println("명령어) " + cmd);  중복으로 제거
+
             if (cmd.length() == 0) {
                 System.out.println("명령어를 입력해 주세요");
                 continue;
@@ -22,15 +21,15 @@ public class Main {
             if (cmd.equals("exit")) {
                 break;
             }
-            if ( cmd.equals("article list")) {
-                if (articles.size() == 0) {           // size() 공부,  누구의 메서드?
-                     System.out.println("게시글이 없습니다");
-                     continue;
+            if (cmd.equals("article list")) {
+                if (articles.size() == 0) {
+                    System.out.println("게시글이 없습니다");
+                    continue;
                 }
                 System.out.println("번호  |   제목");
-                for(int i = articles.size() - 1 ; i >= 0 ; i-- ) {    //articles 역순으로 출력
-                   Article article= articles.get(i);
-                    System.out.printf("%d    |    %s\n" , article.id, article.title);
+                for (int i = articles.size() - 1; i >= 0; i--) {
+                    Article article = articles.get(i);
+                    System.out.printf("%d    |    %s   |  %s  |   %s \n", article.id, article.title, article.body, article.regDate  );  //static 메서드 호출
                 }
             } else if (cmd.equals("article write")) {
                 System.out.print("제목 : ");
@@ -39,46 +38,38 @@ public class Main {
                 String body = sc.nextLine();
                 System.out.println(lastArticleID + " 번글이 생성되었습니다");
 
-//                Article article = new Article();  //default 생성자
-                Article article = new Article(lastArticleID, title, body); // 인자를 통해 생성자 호출
 
-//                article.id = lastArticleID;
-//                article.title = title;
-//                article.body = body;
+                Article article = new Article(lastArticleID, title, body, Util.getDateStr()); // 현재 날짜+시간 불러오기 --> Util.getDataStr()
 
                 articles.add(article);
                 lastArticleID++;
-            } else if (cmd.startsWith("article detail")) { //startswith() 특정 문자열로 문자열 시작? -> trus or false
-                String[] cmdBits = cmd.split(" ");    //split(" ")  "  " 구분자로 문자열 분리해서 배열로 return
-//                System.out.println(cmdBits[0]);
-//                System.out.println(cmdBits[1]);
-//                System.out.println(cmdBits[2]);
-                Article foundArticle = null ;  // foundArticle 용도는 'null check' 사용
-                int id = 0;  // 변수 id가 try문 추가함으로 인해 지역변수화 --> 선언 위치 조정 --> 조금더 큰 지역변수로 선언/초기화
+            } else if (cmd.startsWith("article detail")) {
+                String[] cmdBits = cmd.split(" ");
+                Article foundArticle = null;
+                int id = 0;
 
                 try {
-                    id = Integer.parseInt(cmdBits[2]); //"1" --> 문자를 숫자로 변환 로직
+                    id = Integer.parseInt(cmdBits[2]);
 
-                } catch (NumberFormatException e) {   // (예외타입 변수명)
+                } catch (NumberFormatException e) {
                     System.out.println("정수를 입력하시길 바랍니다");
                     continue;   // while 다시 실행해
                 } catch (Exception e) {
                     //(그밖에 모든 Exception 변수명)
                 }
-
-                for(Article article : articles) {
+                for (Article article : articles) {
                     if (article.id == id) {
                         foundArticle = article;   //search 성공시 article 객체를 --> foundArticle 대입
                         break;  //for문을 빠져나감
                     }
                 }
                 if (foundArticle == null) { //serarch 수행했으나 게시글이 없음
-                    System.out.printf("%d번 게시물이 존재하지 않습니다\n" , id);
+                    System.out.printf("%d번 게시물이 존재하지 않습니다\n", id);
                     continue; //while문을 다시 시작해라
                 }
                 // serach후 detail 내용 출력
                 System.out.println("번호 : " + foundArticle.id);
-                System.out.println("날짜 : ~~~");
+                System.out.println("날짜 : " + foundArticle.regDate); //날짜 + 시간 출력
                 System.out.println("제목 : " + foundArticle.title);
                 System.out.println("내용 : " + foundArticle.body);
             } else {
@@ -88,14 +79,17 @@ public class Main {
         System.out.println("== 프로그램 종료 ==");
     }
 }
+
 class Article {
     int id;
     String title;
     String body;
+    String regDate;  // 필드 추가 --> yyyy-mm-dd hh:mm:ss
 
-    public Article(int lastArticleID, String title, String body) {  //생성자를 통해서 초기화 작업
-                this.id = lastArticleID;
-                this.title = title;
-                this.body = body;
+    public Article(int lastArticleID, String title, String body, String regDate) {
+        this.id = lastArticleID;
+        this.title = title;
+        this.body = body;
+        this.regDate = regDate;    // system(PC) 날짜 + 시간 이 String 저장
     }
 }
